84999ce884d8ced442a947ba458e9f6a9cefa56e
Perry Ellis <pdellis@ncsu.edu>
2026-03-04 16:03:11 -0500

    Added A record specifier in url2dns dig command

--- scripts/url2dns.sh
+++ scripts/url2dns.sh
@@ -2,6 +2,6 @@
 
 # Returns only A IP address records for all hostnames in a file passed in arg1.
 
-(dig -f $1 +short | grep -P "^([0-9]+\.?){4}$" | sort | uniq)
+(dig -f $1 A +short | grep -P "^([0-9]+\.?){4}$" | sort | uniq)
 
 exit 0
